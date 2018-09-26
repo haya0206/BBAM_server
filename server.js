68bb0124108f8654525c503b77894a25142ffabd
haya0206 <haya0206@naver.com>
2018-09-26 17:10:54 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.post("/asdf", (req, res) => {
+  console.log(req.body); //undefined
+  res.status(200).json("success");  
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
