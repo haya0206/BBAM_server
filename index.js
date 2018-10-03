315c91df6d01a1654ecabb81c07ed62d1037f88b
SungBeom <psb_94@naver.com>
2018-10-03 13:24:18 +0900

    Sequelize DB 연동

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+    'BBAM',
+    'root',
+    'cosmos0',
+    {
+        'host': 'localhost', // host를 AWS 서버로 변경해야함
+        'dialect': 'mysql',
+        define: {
+            freezeTableName: true
+        }
+    }
+);
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch(err => {
+    console.error('Unable to connect to database:', err);
+});
+
+const GM_INFO = sequelize.define('GM_INFO', {
+    GM_ID: {
+        type: Sequelize.STRING(10),
+        primaryKey: true,
+        allowNull: false
+    },
+    GM_LV: {
+        type: Sequelize.TINYINT,
+        defaultValue: 1
+    },
+    GM_EXP: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+    },
+    GM_RTN: {
+        type: Sequelize.INTEGER
+    }
+}, {
+    timestamps: false
+});
+
+GM_INFO.sync({force: false}).then(() => {
+    return GM_INFO.create({
+        GM_ID: 'TEST',
+        GM_RTN: 1000
+    });
+});
+
+// GM_INFO.create({
+//     GM_ID: 'TEST',
+//     GM_RTN: 1000
+// });
+
+// GM_INFO.findAll().then(users => {
+//     console.log(users);
+// })
+
 // const express = require("express");
 // var router = express.Router();
 
@@ -23,16 +81,16 @@
 // });
 // module.exprots = router;
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'cosmos0',
-  port     : 3306,
-  database : 'BBAM'
-});
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'cosmos0',
+//   port     : 3306,
+//   database : 'BBAM'
+// });
 
-connection.connect();
+// connection.connect();
 
 // // 사용자 신상 정보 보기
 // connection.query('SELECT * FROM USR_INFO ORDER BY USR_JNDT ASC', function(err, rows, fields) {
@@ -57,14 +115,14 @@ connection.connect();
 // });
 
 // 문제 정보 보기
-connection.query('SELECT * FROM PRB_INFO', function(err, rows, fields) {
-    if(!err) {
-        console.log("<<문제 정보>>");
-        console.log(rows);
-    }
-    else {
-        console.log('Error while performing Query. - 문제 정보', err);
-    }
-});
+// connection.query('SELECT * FROM PRB_INFO', function(err, rows, fields) {
+//     if(!err) {
+//         console.log("<<문제 정보>>");
+//         console.log(rows);
+//     }
+//     else {
+//         console.log('Error while performing Query. - 문제 정보', err);
+//     }
+// });
 
-connection.end();
+// connection.end();
--- server.js
+++ server.js
@@ -7,31 +7,89 @@ app.use(cors());
 app.use(express.json());
 app.post("/asdf", (req, res) => {
   console.log(req.body); //undefined
+
+  let code = req.body.code.split("\n");
+  let parsingCode = [
+    {
+      ID: 0,
+      UPPER_ID: 0,
+      TYPE: "",
+      VALUE: ""
+    }
+  ];
+  let parentId = [0];
+  let preDepth = 0;
+  const codeLength = code.length;
+  for (let i = 0; i < codeLength - 1; i++) {
+    let parsing = {};
+    // id 설정
+    parsing.ID = i + 1;
+    // depth 계산
+    let depth = 0;
+    const codeLineLength = code[i].length;
+    for (let j = 0; j < codeLineLength; j++) {
+      if (code[i][j] === " " && code[i][j + 1] === " ") {
+        depth++;
+        j++;
+      }
+    }
+    if (depth > preDepth) {
+      parentId.push(i);
+    } else if (depth < preDepth) {
+      parentId.pop();
+    }
+    preDepth = depth;
+    parsing.UPPER_ID = parentId[parentId.length - 1];
+    // type 찾기
+    while (code[i][0] === " ") {
+      code[i] = code[i].slice(2);
+    }
+    const codeSplit = code[i].split(" ");
+    if (codeSplit[0] === "if") {
+      parsing.TYPE = "IF";
+      parsing.VALUE = code[i].slice(3, -1);
+    } else if (codeSplit[0] === "while") {
+      parsing.TYPE = "WHILE";
+      parsing.VALUE = code[i].slice(6, -1);
+    } else if (codeSplit[0] === "for") {
+      parsing.TYPE = "FOR";
+      parsing.VALUE = code[i].slice(4, -1);
+    } else if (code[i].slice(0, 5) === "print") {
+      parsing.TYPE = "PRINT";
+      parsing.VALUE = code[i].slice(6, -1);
+    } else if (codeSplit[1] === "=") {
+      parsing.TYPE = "DEFINE";
+      parsing.VALUE = codeSplit[2];
+    }
+    parsingCode.push(parsing);
+  }
+  console.log(parsingCode);
+  
   res.status(200).json("success");  
 });
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'cosmos0',
-  port     : 3306,
-  database : 'BBAM'
-});
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'cosmos0',
+//   port     : 3306,
+//   database : 'BBAM'
+// });
 
-connection.query('SELECT * FROM USR_INFO', function(err, rows, fields) {
-  if(!err) {
-      console.log("<<문제 정보>>");
-      console.log(rows);
-  }
-  else {
-      console.log('Error while performing Query. - 사용자 신상 정보', err);
-  }
-});
+// connection.query('SELECT * FROM USR_INFO', function(err, rows, fields) {
+//   if(!err) {
+//       console.log("<<문제 정보>>");
+//       console.log(rows);
+//   }
+//   else {
+//       console.log('Error while performing Query. - 사용자 신상 정보', err);
+//   }
+// });
 
-connection.end();
+// connection.end();
 
-connection.connect();
+// connection.connect();
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
